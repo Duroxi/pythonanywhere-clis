3fcad8a96366592fb0aabcf38f7bace10eac7757
CharlesHahn <3447180435@qq.com>
2026-05-28 08:34:49 +0800

    feat: add main CLI entry point with all subcommands

---
+++ pa_cli/cli/main.py
@@ -0,0 +1,22 @@
+import typer
+
+from pa_cli.cli.init_cmd import app as init_app
+from pa_cli.cli.files_cmd import app as files_app
+from pa_cli.cli.consoles_cmd import app as consoles_app
+from pa_cli.cli.webapps_cmd import app as webapps_app
+from pa_cli.cli.deploy_cmd import app as deploy_app
+
+app = typer.Typer(
+    help="CLI tool for automating PythonAnywhere deployments.",
+    no_args_is_help=True,
+)
+
+app.add_typer(init_app, name="init", help="Configure PythonAnywhere account")
+app.add_typer(files_app, name="files", help="Manage files on PythonAnywhere")
+app.add_typer(consoles_app, name="console", help="Manage consoles on PythonAnywhere")
+app.add_typer(webapps_app, name="webapp", help="Manage web apps on PythonAnywhere")
+app.add_typer(deploy_app, name="deploy", help="Deploy a local project to PythonAnywhere")
+
+
+if __name__ == "__main__":
+    app()
